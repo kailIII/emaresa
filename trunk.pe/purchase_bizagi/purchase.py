1645a5dddbb838335bd34fc739afce1b656c3d24
Cubic ERP <info@cubicerp.com>
2015-07-07 20:19:10 -0500

    [ADD] purchase_bizagi module

--- trunk.pe/purchase_bizagi/purchase.py
+++ trunk.pe/purchase_bizagi/purchase.py
@@ -1,4 +1,7 @@
 from openerp.osv import osv, fields
+from pysimplesoap.client import SoapClient
+from pysimplesoap.simplexml import SimpleXMLElement
+
 
 class purchase_journal(osv.Model):
     _name = 'purchase.journal'
@@ -15,3 +18,43 @@ class purchase_order(osv.Model):
             'bitacora_bizagi': fields.text('Bitacora Bizagi', readonly=True),
         }
     
+    def action_send_bizagi(self, cr, uid, ids, context=None):
+        client = SoapClient(wsdl="http://172.17.40.93/Procesos/webservices/WorkflowEngineSOA.asmx?wsdl")
+        for order in self.browse(cr, uid, ids, context=context):
+            result = client.createCasesAsString("""
+            <BizAgiWSParam xmlns="">
+                    <domain>domain</domain>
+                    <userName>admon</userName>
+                    <Cases>
+                        <Case>
+                            <Process>PruebasRCV</Process>
+                            <Entities>
+                                <PruebasRCV>
+                                    <nrosolicitud>%s</nrosolicitud>
+                                    <solicitante>%s</solicitante>
+                                    <tipo>%s</tipo>
+                                    <detalle>
+                                        <detalle>
+                                            <producto>7024000400213</producto>
+                                            <descripcion>PAD CAFE 13"</descripcion>
+                                            <precio>1525</precio>
+                                         </detalle>
+                                         <detalle>
+                                            <producto>7024000400214</producto>
+                                            <descripcion>CADENA 33 MM</descripcion>
+                                            <precio>88542</precio>
+                                         </detalle>
+                                         <detalle>
+                                            <producto>7024000400215</producto>
+                                            <descripcion>DESECHOS TOXICOS</descripcion>
+                                            <precio>5000</precio>
+                                         </detalle>
+                                    </detalle>
+                                </PruebasRCV>
+                            </Entities>
+                        </Case>
+                    </Cases>
+                </BizAgiWSParam>"""%(order.name, order.validator.name, order.purchase_order_id.name))
+            self.write(cr, uid, [order.id], {'bitacora_bizagi': result})
+
+        
