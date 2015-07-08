9b56c9eebd14fe02578de2ae027c04487a2e25ef
Cubic ERP <info@cubicerp.com>
2015-07-08 12:06:55 -0500

    [ADD] purchase_bizagi module

--- trunk.pe/purchase_bizagi/purchase.py
+++ trunk.pe/purchase_bizagi/purchase.py
@@ -85,6 +85,6 @@ class purchase_order(osv.Model):
                                      detalle,
                                      order.id))
             self.write(cr, uid, [order.id], {'bitacora_bizagi': result, 
-                                             'date_confirm': fields.date.context_today('purchase.order', cr, uid, context=context)})
+                                             'date_confirm': fields.date.context_today(self, cr, uid, context=context)})
 
         
