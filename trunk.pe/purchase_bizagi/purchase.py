5893deca471641d8d88bb91053316da4a5e706c3
Cubic ERP <info@cubicerp.com>
2015-07-07 19:21:51 -0500

    [ADD] purchase_bizagi module

---
+++ trunk.pe/purchase_bizagi/purchase.py
@@ -0,0 +1,16 @@
+from openerp.osv import osv, fields
+
+class purchase_journal(osv.Model):
+    _name = 'purchase.journal'
+    _columns = {
+            'name': fields.char('Name', size=32),
+        }
+    
+class purchase_order(osv.Model):
+    _name = "purchase.order"
+    _inherit = "purchase.order"
+    _columns = {
+            'purchase_journal_id': fields.many2one('purchase.journal', string="Purchase Journal", required=True),
+            'bitacora_bizagi': fields.text('Bitacora Bizagi', readonly=True),
+        }
+    
